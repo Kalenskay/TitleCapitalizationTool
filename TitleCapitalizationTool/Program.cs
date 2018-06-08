4ffc602f72ee0dbaadb2d5d4107db6493fdfd6da
shadowthief77 <shadowthief77@gmail.com>
2018-06-08 09:47:31 +0300

    начальный проэкт

--- TitleCapitalizationTool/Program.cs
+++ TitleCapitalizationTool/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TitleCapitalizationTool
 {
@@ -10,6 +7,51 @@ namespace TitleCapitalizationTool
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter title to capitalize:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                string message = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Capitalized title: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                string message2 = (message.Replace(".", " . "));
+                message2 = (message2.Replace(",", " , "));
+                message2 = (message2.Replace(";", " ; "));
+                message2 = (message2.Replace(":", " : "));
+                message2 = (message2.Replace("!", " ! "));
+                message2 = (message2.Replace("?", " ? "));
+                message2 = (message2.Replace("-", " - "));
+
+                string[] arrayMessage = message2.Split(' ');
+                StringBuilder finalMessage = new StringBuilder();
+                foreach (string word in arrayMessage)
+                {
+                    if (",.;:!?-".Contains(word))
+                    {
+                        if (word == "-")
+                        {
+                            finalMessage.Append(" ");
+                        }
+                        finalMessage.Append(word);
+                    }
+                    else
+                    {
+                        string word2 = word.ToLower();
+                        if (!"a an the and but for nor so yet at by in of on or out to up".Contains(word2) || finalMessage.Length == 0)
+                        {
+                            word2 = Char.ToUpper(word2[0]) + word2.Substring(1);
+                        }
+                        finalMessage.Append(" ");
+                        finalMessage.Append(word2);
+                    }
+                }
+
+                message2 = (finalMessage.ToString().Trim());
+                Console.WriteLine(message2);
+            }
         }
     }
 }
